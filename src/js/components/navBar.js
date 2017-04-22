f913182ac41b54e6b3b5a8667b65cb90db600467
andela-onnenanya <obinna.nnenanya@andela.com>
2017-04-22 21:35:30 +0000

    remove unwanted elements

--- src/js/components/navBar.js
+++ src/js/components/navBar.js
@@ -12,19 +12,8 @@ class Navbar extends React.Component {
             <nav className="navbar navbar-inverse">
               <div className="container-fluid">
                 <div className="navbar-header">
-                  <a className="navbar-brand" href="#">WebSiteName</a>
+                  <a className="navbar-brand" href="#">gistMe</a>
                 </div>
-                <ul className="nav navbar-nav">
-                  <li className="active"><a href="#">Home</a></li>
-                  <li className="dropdown"><a className="dropdown-toggle" data-toggle="dropdown" href="#">Page 1 <span className="caret"></span></a>
-                    <ul className="dropdown-menu">
-                      <li><a href="#">Page 1-1</a></li>
-                      <li><a href="#">Page 1-2</a></li>
-                      <li><a href="#">Page 1-3</a></li>
-                    </ul>
-                  </li>
-                  <li><a href="#">Page 2</a></li>
-                </ul>
                 <ul className="nav navbar-nav navbar-right">
                   <Logout />
                 </ul>
