185cd543b62178aa8d247240183cc50f3fb6d847
Andela Developer <andeladeveloper@Andelas-MacBook-Pro.local>
2017-04-29 10:00:27 +0100

    finish test for NavBar component

--- test/components/NavBar.spec.jsx
+++ test/components/NavBar.spec.jsx
@@ -8,24 +8,24 @@ describe('NavBar component', () => {
   it('should exist', () => {
     expect(<NavBar />).toBeDefined();
   });
-  
-  it('should render a "div" element as the wrapper', () => {
-    const wrapper = mount(<NavBar />);
-    console.log(wrapper.find('nav').parent())
-    expect(wrapper.find('nav').length).toEqual(1);
+
+  it('should mount and render a "nav" element as the first child', () => {
+    const MountWrapper = mount(<NavBar />);
+    expect(MountWrapper.children().first().is('nav')).toBe(true);
+    expect(MountWrapper.find('nav').parent().is('NavBar')).toEqual(true);
   });
-  
+
   it('should accept props', () => {
-    const wrapper = mount(<NavBar test='props'/>);
+    const wrapper = mount(<NavBar test="props" />);
     expect(wrapper.props().test).toEqual('props');
-    wrapper.setProps({ test: "props2" });
+    wrapper.setProps({ test: 'props2' });
     expect(wrapper.props().test).toEqual('props2');
   });
 
   it('should mount in a full DOM', () => {
     expect(mount(<NavBar />).find('.header').length).toBe(1);
   });
-  
+
   it('should render child component', () => {
     expect(mount(<NavBar />).find('.navbar').length).toBeGreaterThan(0);
     expect(mount(<NavBar />).find('Logout').length).toBe(1);
