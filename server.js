d50db6389f8f7aaba89590970be8cdb217c133ac
Andela Developer <andeladeveloper@Andelas-MacBook-Pro.local>
2017-05-06 19:59:47 +0100

    clean-up code

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 require('dotenv').load();
 const express = require('express');
 const path = require('path');
-const  mongoose = require('mongoose'),
-  passport = require('passport'),
-  session = require('express-session'),
-  MongoStore = require("connect-mongo")(session);
-const MONGODB_URI=process.env.MONGODB_URI;
+const mongoose = require('mongoose');
+const passport = require('passport');
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
+
+const MONGODB_URI = process.env.MONGODB_URI;
 
 
 const app = express();
@@ -14,30 +15,22 @@ app.use('', express.static(path.join(__dirname, 'public')));
 app.use('/dist', express.static(path.join(__dirname, 'dist')));
 app.use('/fa', express.static(path.join(__dirname, 'fa')));
 
-function home(req, res){
-  if(req.isAuthenticated()){
-    res.redirect('/news');
-  }else{
-    return '/';
-  }
-}
-
-
 require('./config/passport')(passport);
+
 mongoose.connect(MONGODB_URI);
 
-var sess = {
-        secret: 'keyboard cat',
-        resave: false,
-        saveUninitialized: false,
-        cookie: {},
-        store: new MongoStore({ mongooseConnection: mongoose.connections[0] })
-    }
+const sess = {
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: false,
+  cookie: {},
+  store: new MongoStore({ mongooseConnection: mongoose.connections[0] }),
+};
 
 if (app.get('env') === 'production') {
-    app.set('trust proxy', 1) // trust first proxy
-    sess.cookie.secure = true; // serve secure cookies
-    sess.cookie.maxAge = 1000 * 60 * 60 * 14; //14 days
+  app.set('trust proxy', 1);  // trust first proxy
+  sess.cookie.secure = true; // serve secure cookies
+  sess.cookie.maxAge = 1000 * 60 * 60 * 14; // 14 days
 }
 
 app.use(session(sess));
@@ -48,38 +41,45 @@ app.get('/', (req, res) => {
   if (req.isAuthenticated()) {
     res.redirect('/news');
   } else {
+    /*eslint-disable*/
+    console.log("Home Called!");
+    /*eslint-enable*/
     res.sendFile(path.resolve(__dirname, 'public', 'home.html'));
   }
 });
 
 app.get('/news', (req, res) => {
   if (req.isAuthenticated()) {
+    /*eslint-disable*/
+    console.log("News Called!");
+    /*eslint-enable*/
     res.sendFile(path.resolve(__dirname, 'public', 'newsPage.html'));
   } else {
     res.redirect('/');
   }
 });
 
-//Google+ login route
+// Google+ login route
 
 // authenticate user on google
 app.route('/auth/google')
   .get(passport.authenticate('google', { scope: ['profile', 'email'] }));
 
-//OAuth response
+// OAuth response
 app.route('/auth/google/callback')
   .get(
     passport.authenticate('google', {
-    successRedirect: '/news',
-    failureRedirect: '/'
+      successRedirect: '/news',
+      failureRedirect: '/',
     })
   );
 
 app.route('/logout')
-  .get(function (req, res) {
+  .get((req, res) => {
     req.logout();
     res.redirect('/');
   });
 
+/*eslint-disable*/
 app.listen(process.env.PORT || 8080);
 console.log(`${process.env.PORT || 8080} is the magic port`);
