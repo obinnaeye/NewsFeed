fc0b09e3ee20b1a0172f19dbba3b4464a39020db
Andela Developer <andeladeveloper@Andelas-MacBook-Pro.local>
2017-05-07 22:57:40 +0100

    checkout error

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const path = require('path');
+
+const app = express();
+// middleware for static files
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/dist', express.static(path.join(__dirname, 'dist')));
+app.use('/fa', express.static(path.join(__dirname, 'fa')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
+});
+
+app.listen(process.env.PORT || 8080);
+console.log(`${process.env.PORT || 8080} is the magic port`);
