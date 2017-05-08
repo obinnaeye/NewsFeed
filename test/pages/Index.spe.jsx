998d0682f6c24e8d04b8e9c68667b2c12f618601
Andela Developer <andeladeveloper@Andelas-MacBook-Pro.local>
2017-05-08 14:57:37 +0100

    fix error with module import

--- test/pages/Index.spe.jsx
+++ test/pages/Index.spe.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { mount } from 'enzyme';
 
+console.log("Here");
+
 import Index from '../../src/js/pages/Index';
 /*eslint-disable */
 describe('Index page', () => {
